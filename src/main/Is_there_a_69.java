6e84339c85841d170c821a3fef2fad65c472a121
Klemen-Z <klemenzmahar@gmail.com>
2022-01-12 15:14:57 +0100

    Simplified program to be an Object and not static

--- src/main/Is_there_a_69.java
+++ src/main/Is_there_a_69.java
@@ -1,17 +1,7 @@
 package main;
 
-import java.util.Scanner;
-
 public class Is_there_a_69 {
-    static Scanner sc = new Scanner(System.in);
-
-    public static void main(String[] args) {
-        System.out.println("Insert String to check for 69");
-        String str = sc.nextLine();
-        if(str.contains("69")){
-            System.out.println("The input contains 69 starting at char (no spaces counted): " + str.indexOf("69"));
-        } else {
-            System.out.println("The input does not contain 69");
-        }
+    public boolean checker(String str) {
+        return str.contains("69");
     }
 }
